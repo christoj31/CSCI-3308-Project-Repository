f541ca62c38578a39ce6d2d8def0d1e5fe899d38
Christopher Jewett <chje6872@colorado.edu>
2024-10-29 15:10:26 -0600

    sql

---
+++ ProjectSourceCode/src/init_data/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS leeds (
+    job_id INT PRIMARY KEY,
+    job_name VARCHAR(50) NOT NULL,
+    company VARCHAR(50) NOT NULL,
+    date_applied VAR NOT NULL,
+    status VARCHAR(60) NOT NULL
+);
---
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO leeds 
+    (job_id, job_name, company, date_applied, status)
+VALUES 
+    (
+        '0001',
+        'Intern',
+        'Google',
+        '10/29/2024',
+        'Applied'
+    );
