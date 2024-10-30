b6377b5afaabdd07cf7dcbefa79d5c9361498776
Christopher Jewett <chje6872@colorado.edu>
2024-10-30 10:49:16 -0600

    Creating database updates

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -7,13 +7,57 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Set Handlebars as the view engine
-app.engine('hbs', exphbs({
+
+// const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
+
+const hbs = exphbs.create({
+    extname: '.hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    defaultLayout: 'main'
+});
+
+app.engine('hbs', hbs.engine);
+
+/*
+const dbConfig = {
+    host: 'db', 
+    port: 5432,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER, 
+    password: process.env.POSTGRES_PASSWORD,
+  };
+  
+const db = pgp(dbConfig);
+  
+  db.connect()
+    .then(obj => {
+      console.log('Database connection successful'); // you can view this message in the docker compose logs
+      obj.done(); // success, release the connection;
+    })
+    .catch(error => {
+      console.log('ERROR:', error.message || error);
+    });
+*/
+  
+/*
+app.engine('hbs', express({
     extname: '.hbs',
     layoutsDir: path.join(__dirname, 'views/layouts'), // Adjust path if needed
     defaultLayout: 'main'
 }));
+*/
+
 app.set('view engine', 'hbs');
+
 app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.redirect('/login');
+});
+
+app.get('/login', (req, res) => {
+    res.render('pages/login');
+});
 
 // Use the theme controller for routes
 app.use('/', themeController);
