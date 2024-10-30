56aaf25c66a9bf4598f171d028762d0d06676871
Christopher Jewett <chje6872@colorado.edu>
2024-10-30 09:22:12 -0600

    Updates around docker compose

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,7 +1,8 @@
+DROP TABLE IF EXISTS leeds CASCADE;
 CREATE TABLE IF NOT EXISTS leeds (
-    job_id INT PRIMARY KEY,
-    job_name VARCHAR(50) NOT NULL,
-    company VARCHAR(50) NOT NULL,
-    date_applied VAR NOT NULL,
-    status VARCHAR(60) NOT NULL
+  job_id SERIAL PRIMARY KEY NOT NULL,
+  job_name VARCHAR(50) NOT NULL,
+  company VARCHAR(50) NOT NULL,
+  date_applied VAR NOT NULL,
+  status VARCHAR(60) NOT NULL
 );
