059dff9a60a5c605d8eebe44d35d1ec07a07e708
Christopher Jewett <chje6872@colorado.edu>
2024-10-31 15:53:12 -0600

    insert update

--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -1,14 +1,3 @@
-INSERT INTO leeds 
-    (job_id, job_name, company, date_applied, status)
-VALUES 
-    (
-        '0001',
-        'Intern',
-        'Google',
-        '10/29/2024',
-        'Applied'
-    );
-
 INSERT INTO users
     (userID, username, password, email, phoneNumber)
 VALUES 
