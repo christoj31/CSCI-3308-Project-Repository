5d8a6d9d5c4a8fe2f2b10b46490cc71394407089
Daria Ruchala <daria.ruchala@colorado.edu>
2024-10-31 15:23:19 -0600

    CRAETE TABLE

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -6,3 +6,78 @@ CREATE TABLE IF NOT EXISTS leeds (
   date_applied VARCHAR NOT NULL,
   status VARCHAR(60) NOT NULL
 );
+
+CREATE TABLE users (
+userID INT PRIMARY KEY,
+username VARCHAR (45),
+password VARCHAR (45),
+email VARCHAR (45),
+phoneNumber VARCHAR (45)
+);
+
+CREATE TABLE resume (
+resumeID INT PRIMARY KEY,
+resumeStage VARCHAR(45)
+);
+
+CREATE TABLE company (
+companyID INT PRIMARY KEY,
+name VARCHAR (45),
+industry VARCHAR (45),
+location VARCHAR (45)
+);
+
+CREATE TABLE point0fContact (
+pocID INT PRIMARY KEY,
+firstName VARCHAR (45),
+lastName VARCHAR (45),email VARCHAR (45),
+phoneNumber VARCHAR (45)
+);
+
+CREATE TABLE jobs (
+jobID INT PRIMARY KEY,
+jobTitle VARCHAR (45),
+jobApplicationLink LONGTEXT,
+applicationStep VARCHAR (45),
+resumeID INT
+pOcID INT
+attemptID INT,
+FOREIGN KEY (resumeID) REFERENCES Resume (resumeID),
+FOREIGN KEY (pocID) REFERENCES point0fContact (pocID),
+FOREIGN KEY (attemptID) REIENCES contactAttempt (attemptID)
+);
+
+CREATE TABLE userJobs (
+userID INT,
+jobID INT.
+PRIMARY KEY (userID, jobID),
+FOREIGN KEY (userID) REFERENCES users (userID),
+FOREIGN KEY (jobID) REFERENCES jobs (jobID)
+); 
+
+CREATE TABLE contactAttempt (
+attemptID INT PRIMARY KEY,
+contacted TINYINT,
+contactDate DATETIME,
+followUpDate DATETIME,
+meetingID INT,
+location VARCHAR (45),
+FOREIGN KEY (meetingID) 
+REFERENCES Meetings (meetingID)
+);
+
+CREATE TABLE Meetings (
+meetingID INT PRIMARY KEY,
+meet ingDate DATETIME,
+thankYouSent TINYINT
+);
+
+CREATE TABLE TrackingTable (
+userCount INT,
+jobCount INT
+resumeCount INT,
+companyCount INT,
+point0fContactCount INT,
+contactAttemptCount INT,
+Meet ingCount INT
+);
