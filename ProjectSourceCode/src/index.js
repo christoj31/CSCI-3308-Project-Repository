f00e1b2d0318faac4f112a6146ff486c3b10f6ac
yuchanandrew <anrh8554@colorado.edu>
2024-11-15 01:54:28 -0700

    Final touches to modal

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -239,25 +239,31 @@ app.post('/home', async (req, res) => {
         // const due_date = req.body.due_date;
         //const status = req.body.status;
 
-        // const query = 'SELECT jobID FROM jobs WHERE jobID = $1 LIMIT 1';
-        // const check_values = [jobID];
+        const query = 'SELECT jobID FROM jobs WHERE jobID = $1 LIMIT 1';
+        const check_values = [jobID];
 
-        // const job_exists = await db.oneOrNone(query, check_values);
-        // if (job_exists) {
-        //         return res.status(400).render('pages/home', {
-        //             error: 'Job already listed.'
-        //         });
-        //     }
+        const job_exists = await db.oneOrNone(query, check_values);
+        if (job_exists) {
+                return res.status(400).render('pages/home', {
+                    error: 'Job already listed.'
+                });
+            }
+        
+        const check_table_empty_query = 'SELECT COUNT(*) FROM jobs';
+        const empty = await db.one(check_table_empty_query);
+        const isTableEmpty = empty.count === '0';
 
-        // const generate_job_id_query = 'SELECT jobID FROM jobs ORDER BY jobID DESC LIMIT 1';
-        // let result = await db.one(generate_job_id_query);
-        // let job_id = result.jobID;
-        // job_id += 1; 
+        let job_id = 1; // Default job_id
+        if (!isTableEmpty) {
+            const generate_job_id_query = 'SELECT jobID FROM jobs ORDER BY jobID DESC LIMIT 1';
+            const result = await db.one(generate_job_id_query);
+            job_id = result.jobID + 1;
+        }
 
-        // const insert_query = 'INSERT INTO jobs (jobID, jobTitle, jobApplicationLink, due_date) VALUES ($1, $2, $3, $4)';
-        const insert_query = 'INSERT INTO jobs (jobTitle, jobApplicationLink) VALUES ($1, $2)';
-        // const insertValues = [job_id, job_name, job_link, due_date];
-        const insertValues = [job_name, job_link];
+        const insert_query = 'INSERT INTO jobs (jobID, jobTitle, jobApplicationLink, due_date) VALUES ($1, $2, $3, $4)';
+        // const insert_query = 'INSERT INTO jobs (jobTitle, jobApplicationLink) VALUES ($1, $2)';
+        const insertValues = [job_id, job_name, job_link, due_date];
+        // const insertValues = [job_name, job_link];
 
         await db.none(insert_query, insertValues);
 
