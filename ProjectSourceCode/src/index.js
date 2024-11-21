4af89844f34609479f985d2d8a6a9dee99cdc2c8
Christopher Jewett <chje6872@colorado.edu>
2024-11-21 16:56:08 -0700

    poc tooltip

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -30,6 +30,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 // FOR LAB 13
+/*
 const dbConfig = {
     host: 'acae94f1d885f4543297873c90ffcf9b', 
     port: 5432,
@@ -38,14 +39,15 @@ const dbConfig = {
     password: 'Lead2025@',
     secret: '3758ec009de5d9fabbaaa8f6e2b140c0'
   };
+*/
   
-// const dbConfig = {
-//     host: 'db', 
-//     port: 5432,
-//     database: process.env.POSTGRES_DB,
-//     user: process.env.POSTGRES_USER, 
-//     password: process.env.POSTGRES_PASSWORD,
-//   };
+const dbConfig = {
+     host: 'db', 
+     port: 5432,
+     database: process.env.POSTGRES_DB,
+     user: process.env.POSTGRES_USER, 
+     password: process.env.POSTGRES_PASSWORD,
+   };
 
   
 const db = pgp(dbConfig);
